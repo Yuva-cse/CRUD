bdf2521112eb0cf5ed67b2eeeeff66a267f93fdc
Yuva-cse <144908966+Yuva-cse@users.noreply.github.com>
2023-09-28 17:00:07 +0530

    Unwanted lines are removed

--- models/insertModel.php
+++ models/insertModel.php
@@ -11,42 +11,20 @@ class insertModel extends CI_Model{
         else{
             echo "Error while insert data";
         }
-        // public function back(){
-        //     return;
-        // }
-
-        //print_r($data);
-        //echo "you accesed";
     }
     public function getData(){
         $this->load->database();
-        // echo "you accesed";
         //$result=$this->db->select('name')->get('senchola')->result();
         // the above code is used to fetch a particular data data from database
         return $this->db->get('senchola')->result();
-        // echo '<pre>';
-        // print_r($result);
-        // echo '</pre>';
-        
     }
     public function editData($id){
         $this->load->database();
         return $this->db->where('id',$id)->get('senchola')->result();
-        // echo '<pre>';
-        // print_r($result);
-        // echo '</pre>';
     }
     public function updateData($data,$Id){
-
         $this->load->database();
-        //print_r($data);
-        // $name=$data['name'];
-        // $domain=$data['domain'];
-        // $volunteer_name=$data['volunteer_name'];
-        // $email=$data['email'];
-        // $phone=$data['phone'];
         $suc=$this->db->where('id',$Id)->update('senchola',$data);
-       // $suc=$this->db->query("update senchola SET name='$name',domain='$domain',volunteer_name='$volunteer_name',email='$email',phone='$phone' where id='$Id'");
         if($suc){
             echo '<h3>Updated successfully!</h3><br>';
             $click="Go to home";
@@ -60,7 +38,6 @@ class insertModel extends CI_Model{
         $this->load->database();
         $this->db->where("id", $id);
         $suc=$this->db->delete("senchola");
-        //$this->db->query();
         if($suc){
             echo '<h3>Deleted successfully!</h3><br>';
             $click="Go to home";
